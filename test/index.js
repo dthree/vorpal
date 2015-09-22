63bf825e99ac3b85356fd885a5546c968e9683b0
dthree <threedeecee@gmail.com>
2015-09-22 12:01:25 -0700

    exposed minimist parsing to .parse

--- test/index.js
+++ test/index.js
@@ -1,13 +1,30 @@
-var vorpal = require('../');
+var Vorpal = require('../');
 var should = require('should');
+
+var vorpal = new Vorpal();
 
 require('assert');
 
 describe('vorpal', function () {
   describe('constructor', function () {
     it('should exist and be a function', function () {
-      should.exist(vorpal);
-      vorpal.should.be.type('function');
+      should.exist(Vorpal);
+      Vorpal.should.be.type('function');
+    });
+  });
+
+  describe('.parse', function () {
+    it('should exist and be a function', function () {
+      should.exist(vorpal.parse);
+      vorpal.parse.should.be.type('function');
+    });
+
+    it('should expose minimist', function () {
+      var result = vorpal.parse(['a', 'b', 'foo', 'bar', '-r'], {use: 'minimist'});
+      result.r.should.be.true;
+      (result._.indexOf('foo') > -1).should.be.true;
+      (result._.indexOf('bar') > -1).should.be.true;
+      result._.length.should.equal(2);
     });
   });
 });
