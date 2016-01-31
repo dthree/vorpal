d365b515cb0485d129983e38b4c78a2769460127
Scott Hovestadt <scott.hovestadt@gmail.com>
2016-01-31 03:46:33 -0800

    Implement Babel 6 #86

---
+++ gulpfile.babel.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const _gulp = require('load-plugins')('gulp-*');
+
+const paths = {};
+paths.src = './lib/**/*.js';
+paths.dist = './dist';
+
+gulp.task('lint', () => {
+  return gulp.src(paths.src)
+    .pipe(_gulp.xo());
+});
+
+gulp.task('build', ['lint'], () => {
+  return gulp.src(paths.src)
+    .pipe(_gulp.babel())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.src, ['build']);
+});
+
+gulp.task('default', ['watch']);
