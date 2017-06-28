a4a7b4f51fdc17972c3c70fc0380f014cae7072c
Miles Johnson <mileswjohnson@gmail.com>
2017-06-27 20:07:00 -0700

    Added flow. Reintegrated babel.

--- examples/prompt/prompt.js
+++ examples/prompt/prompt.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var vorpal = require('./../../')();
+var vorpal = require('../../dist/vorpal')();
 
 vorpal.command('login', 'Login (u: root p: vorpal)')
   .action(function (args, cb) {
