fb4bcc97e5e1457d04068acdd61ab8d012f09b7e
Olegzandr VD <alex@oresoftware.com>
2017-07-09 12:37:37 -0700

    added autocompletion example

---
+++ examples/autocompletion/tab-auto-completion.js
@@ -0,0 +1,28 @@
+
+
+const fs = require('fs');
+const path = require('path');
+const vorpal = require('vorpal')();
+
+vorpal.command('run [file]')
+.description('run a single test script')
+.autocomplete({
+  data: function (input, cb) {
+
+  const basename = path.basename(input);
+  const dir = path.dirname(path.resolve(projectRoot + `/${input}`));
+
+  fs.readdir(dir, function (err, items) {
+    if (err) {
+      return cb(null);
+    }
+    const matches = items.filter(function (item) {
+      return String(item).match(basename);
+    });
+
+    return cb(matches);   // vorpal handles autocompletion for you, just pass back the matches
+
+  });
+
+}
+});
