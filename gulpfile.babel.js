c8425300c3f45db506c0da3dd90871cb2a95b1d9
Ore Landau <ore.landau@ironsrc.com>
2018-10-28 01:51:21 +0300

    gulp-typescript

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,32 @@
-const gulp = require('gulp');
-const _gulp = require('load-plugins')('gulp-*');
+const gulp = require('gulp')
+const _gulp = require('load-plugins')('gulp-*')
+var ts = require('gulp-typescript')
+var tsProject = ts.createProject('tsconfig.json');
 
-const paths = {};
-paths.src = './lib/**/*.js';
-paths.dist = './dist';
+const paths = {}
+paths.src = './lib/**/*.js'
+paths.dist = './dist'
 
 gulp.task('lint', () => {
-  return gulp.src(paths.src)
-    .pipe(_gulp.xo());
-});
+    return gulp.src(paths.src)
+        .pipe(_gulp.xo())
+})
 
-gulp.task('build', () => {
-  return gulp.src(paths.src)
-    .pipe(_gulp.babel())
-    .pipe(gulp.dest(paths.dist));
-});
+gulp.task('build', ['build:ts', 'build:babel'])
+
+gulp.task('build:ts', () => {
+    return tsProject.src().pipe(tsProject()).js
+        .pipe(gulp.dest(paths.dist))
+})
+
+gulp.task('build:babel', () => {
+    return gulp.src(paths.dist)
+        .pipe(_gulp.babel())
+        .pipe(gulp.dest(paths.dist))
+})
 
 gulp.task('watch', ['build'], () => {
-  gulp.watch(paths.src, ['build']);
-});
+    gulp.watch(paths.src, ['build'])
+})
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'])
